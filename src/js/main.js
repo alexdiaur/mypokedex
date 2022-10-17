d23a109680a00fa002b7749f47122ef0452e952c
Alejandra Diaz <francys.diazurribarri@gmail.com>
2022-10-17 14:55:34 +0200

    adds listen btns events and get info from api

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,10 @@ let initialPokemon = [];
 let pokemon = [];
 let favorites = [];
 
-const searchText = document.querySelector('.js_searchText');
+const url = `https://pokeapi.co/api/v2/pokemon/`;
+const searchInput = document.querySelector('.js_searchInput');
 const searchBtn = document.querySelector('.js_searchBtn');
+const resetClear = document.getElementById('js_resetBtn');
 const errorMsg = document.querySelector('.js_errorMsg');
 const listBox = document.querySelector('.js_listBox');
 const favsMenuBtn = document.querySelector('.js_showFavs');
@@ -14,52 +16,41 @@ const favoritesIcon = document.querySelector('.js_favsIcon');
 const favsBox = document.querySelector('.js_favs');
 const searchesIcon = document.querySelector('.js_searchesIcon');
 const favsList = document.querySelector('.js_favsList');
-const deleteAllFavoritesButton = document.querySelector('.js_resetFavs');
-const searchBox = document.querySelector('.js_searchBox');
+const deleteFavsBtn = document.querySelector('.js_resetFavs');
+const appNode = document.querySelector('.js_searchBox');
 const searchList = document.querySelector('.js_searchList');
 const searchTitle = document.querySelector('.js_searchTitle');
 
-function pokemonsFromApi() {
-  const url_server = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`;
-
-  fetch(url_server)
-    .then((response) => response.json())
-    .then((data) => {
-      const pokemons = data.drinks;
-
-      initialPokemon = pokemons.map((item) => {
-        return {
-          id: pokemon.id,
-          name: pokemon.name,
-          image: pokemon.sprites.back_default,
-        };
-      });
-
-      // Función to generate a random number to sort the cocktails randomly
-      const randomPokemon = () => Math.random() - 0.5;
-
-      const randominitialPokemon = initialPokemon.sort(randomPokemon);
-
-      const reducedRandominitialPokemon = randominitialPokemon.splice(7, 20);
-
-      initialPokemon = reducedRandominitialPokemon;
-
-      renderinitialPokemon();
-      renderFavorites(favorites);
-
-      searchTitle.innerHTML = 'Algunos cócteles';
+function getPokemons(e) {
+  e.preventDefault();
+  window
+    .fetch(`${url}${searchInput.value.toLowerCase()}`)
+    .then((response) => {
+      if (response.status === 404) {
+        alert('Upps, try again');
+      } else {
+        return response.json();
+      }
     })
-    .catch(() => {
-      searchContainer.classList.add('hidden');
+    .then((responseJSON) => {
+      const allPokemons = [];
 
-      deletePreviousQueries();
-      deleteErrorMessage();
+      const result = [];
 
-      errorMessage(
-        'No lo intentes. Hazlo o no lo hagas. Pero en este caso inténtalo de nuevo por favor. Hemos tenido un problemilla momentáneo.',
-        './assets/images/noTrys.jpg',
-        'Yoda',
-        'El imperio contrataca - Yoda enseñando a luke a usar la fuerza - "No lo intentes. Hazlo o no lo hagas. Pero no lo intentes."'
-      );
+      for (let pokemonInfo in responseJSON) {
+        result.push([pokemonInfo, responseJSON[pokemonInfo]]);
+      }
+      console.table(result);
     });
 }
+
+function resetPokemons(e) {
+  e.preventDefault();
+  console.log('you reseted');
+}
+
+searchBtn.addEventListener('click', getPokemons);
+searchBtn.addEventListener('touchstart', getPokemons);
+
+resetClear.addEventListener('click', resetPokemons);
+resetClear.addEventListener('touchstart', resetPokemons);
