a70181b8d06d63cf1c7e0ed37504070b9e5d4874
Alejandra Diaz <francys.diazurribarri@gmail.com>
2022-10-19 17:47:47 +0200

    render pokemon image and listen the input submit

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,24 +1,41 @@
 'use strict';
 
+let searchPokemon = [];
 const url = `https://pokeapi.co/api/v2/pokemon/`;
 const form = document.querySelector('.form');
-const input = document.querySelector('.input__search');
-const pokemonImage = document.querySelector('.pokemon__image');
+const input = document.querySelector('.form__search');
+const pokemonImage = document.querySelector('.pokemon__img');
 const pokemonNumber = document.querySelector('.pokemon__number');
 const pokemonName = document.querySelector('.pokemon__name');
-const buttonPrev = document.querySelector('.btn-backward');
-const buttonNext = document.querySelector('.btn-forward');
+const btnBackward = document.querySelector('.btn-backward');
+const btnFordward = document.querySelector('.btn-forward');
 
-const fetchPokemon = async (pokemon) => {
+const getPokemonFromApi = async (pokemon) => {
   const response = await fetch(`${url}${pokemon}`);
 
   if (response.status === 200) {
     const data = await response.json();
     return data;
   } else {
-    alert('Oppps try it again');
+    alert('¡Oppps try it again!');
   }
   console.log(data);
 };
+
+const renderPokemon = async (pokemon) => {
+  const data = await getPokemonFromApi(pokemon);
+
+  if (data) {
+    pokemonImage.src = data.sprites.front_default;
+    input.value = '';
+    searchPokemon = data.id;
+  }
+};
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+});
+
+renderPokemon(searchPokemon);
 
 //# sourceMappingURL=main.js.map
